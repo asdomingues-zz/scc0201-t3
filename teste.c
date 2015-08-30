b81198ab09fc0123a2e2fd0aa0216098aa2ff4c1
asdomingues <asdomingues@users.noreply.github.com>
2015-08-29 23:25:00 -0300

    Adicionados comentários e um teste do tipo free(NULL)

--- teste.c
+++ teste.c
@@ -1,3 +1,12 @@
+/*	Liberando memórias não alocadas e alocando nada na memória
+ *
+ *	Programa para testar qual o comportamento do sistema
+ *	ao alocar memória de tamanho 0 ou liberar um ponteiro
+ *	nulo.
+ *
+ *	Allan S. Domingues | 9293290 | ICMC-USP | 29/08/15
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -12,8 +21,14 @@ int main (int argc, char * argv[]) {
 	if (cheio) {
 		printf("Alocou alguma coisa!\n");
 		free(cheio);
+		cheio = NULL;
 	} else {
 		printf("Não alocou nada.\n");
+	}
+
+	if (cheio == NULL) {
+		fprintf(stderr, "O free de um ponteiro para NULL será feito a seguir!\n");
+		free(cheio);
 	}
 
 	return 0;
